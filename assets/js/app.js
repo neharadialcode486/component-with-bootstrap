e9bb4f2a8b8962496e04cba8f923afdc8e7b43bd
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-10-16 12:49:42 +0530

    add font weight

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,18 @@ const monthlyContent = document.getElementById('monthlyContent');
 const fourMonthsContent = document.getElementById('fourMonthsContent');
 
 monthlyTab.addEventListener('click', () => {
-  monthlyTab.classList.add('bg-yellow', 'text-white');
-  monthlyTab.classList.remove('bg-white', 'text-yellow');
-  fourMonthsTab.classList.remove('bg-yellow', 'text-white');
+  monthlyTab.classList.add('bg-orange', 'text-white');
+  monthlyTab.classList.remove('bg-white', 'text-orange');
+  fourMonthsTab.classList.remove('bg-orange', 'text-white');
   fourMonthsTab.classList.add('bg-white', 'text-black');
   monthlyContent.classList.remove('d-none');
   fourMonthsContent.classList.add('d-none');
 });
 
 fourMonthsTab.addEventListener('click', () => {
-  fourMonthsTab.classList.add('bg-yellow', 'text-white');
+  fourMonthsTab.classList.add('bg-orange', 'text-white');
   fourMonthsTab.classList.remove('bg-white', 'text-black');
-  monthlyTab.classList.remove('bg-yellow', 'text-white');
+  monthlyTab.classList.remove('bg-orange', 'text-white');
   monthlyTab.classList.add('bg-white', 'text-black');
   monthlyContent.classList.add('d-none');
   fourMonthsContent.classList.remove('d-none');
