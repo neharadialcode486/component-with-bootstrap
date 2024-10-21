1a7015e30dbbfde4b265722a0874fc7088ef9af1
mohitgoyat525 <montyits71@gmail.com>
2024-10-21 11:39:16 +0530

    add-changes

---
+++ assets/js/counter.js
@@ -0,0 +1,27 @@
+let counted = false;
+$(window).scroll(function () {
+  if (
+    !counted &&
+    $(window).scrollTop() >
+      $("#amzon-counter").offset().top - window.innerHeight
+  ) {
+    // Iterate over each .count element
+    $(".amzon-count").each(function () {
+      const $this = $(this);
+      $this.prop("Counter", 0).animate(
+        { Counter: $this.data("count") },
+        {
+          duration: 2000,
+          easing: "swing",
+          step: function (now) {
+            $this.text(Math.floor(now).toLocaleString());
+          },
+          complete: function () {
+            $this.text(Math.floor(this.Counter).toLocaleString());
+          },
+        }
+      );
+    });
+    counted = true;
+  }
+});
