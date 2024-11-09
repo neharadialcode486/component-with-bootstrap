2048d6ba03a14d0bce6f6557a64b10cb2ba151a4
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-11-09 16:27:55 +0530

    add overlay

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,7 @@
  // =====sidebar open & close==========
  const sidebar = document.getElementById("sidebar");
  const bodyName = document.getElementById("body");
+ const blackLayer = document.querySelector(".sidebar-overlay")
 
  function OpenSidebar() {
    sidebar.classList.add("left-0");
@@ -11,10 +12,14 @@
    sidebar.classList.remove("left-0");
    bodyName.classList.remove("main_parent");
  }
+ function CloseSidebar() {
+  document.body.classList.remove("sidebar-overlay");
+}
 
  // =======sidebar=toggle=================
  function toggleMenuIcon() {
    const menuIcon = document.querySelector(".menu-icon");
    menuIcon.classList.toggle("rotate");
    sidebar.classList.toggle("left-0");
+   blackLayer.classList.toggle("d-flex")
  }
